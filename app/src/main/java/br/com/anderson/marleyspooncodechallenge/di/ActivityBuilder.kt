29dfc65b43efa641341c139447acf72100d3fe27
Anderson Soares <anderson.r.soares@gmail.com>
2020-10-14 10:21:41 -0300

    fix unused

--- app/src/main/java/br/com/anderson/marleyspooncodechallenge/di/ActivityBuilder.kt
+++ app/src/main/java/br/com/anderson/marleyspooncodechallenge/di/ActivityBuilder.kt
@@ -4,6 +4,7 @@ import br.com.anderson.marleyspooncodechallenge.MainActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
+@Suppress("unused")
 @Module
 abstract class ActivityBuilder {
 
